7d00153e314a0dac0ac7e58d362248ccb6e8a59b
kai-machida <49702001+kai-machida@users.noreply.github.com>
2019-06-07 13:01:50 +0900

    未完成

--- fukusyu0531/Form1.cs
+++ fukusyu0531/Form1.cs
@@ -21,17 +21,18 @@ namespace fukusyu0531
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            Text = MousePosition.X + ", " + MousePosition.Y;
+            Point p = PointToClient(MousePosition);
+
+            label2.Left = p.X-label2.Width/2;
+            label2.Top = p.Y-label2.Height/2;
+
             label1.Left += vx;
             label1.Top += vy;
 
             if (label1.Left < 0)
             {
-                vx = -vx;
-            }
-
-            if (label1.Right > 288)
-            {
-                vx = -vx;
+                vx = Math.Abs(vx);
             }
 
             if (label1.Top < 0)
@@ -39,10 +40,22 @@ namespace fukusyu0531
                 vy = -vy;
             }
 
-            if (label1.Bottom > 260)
+            if (label1.Left > ClientSize.Width - label1.Width)
+            {
+                vx = -Math.Abs(vx);
+            }
+
+            if (label1.Top > 247)
             {
                 vy = -vy;
             }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("" + ClientSize.Width + ", " + ClientSize.Height);
+            MessageBox.Show("" + label1.Width + ", " + label1.Height);
         }
     }
 }
